3d2827d58336c1c0c1ccebc5d345e8c369068542
Võ Xuân Sang <121625194+thieu-nu-hoa-hong-z00z@users.noreply.github.com>
2025-04-04 20:42:18 +0700

    Delete Backend directory

--- Backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://dinhviethoang1904:hoang1904@cluster0.xppeknd.mongodb.net/food-del').then(() => console.log('MongoDB Connected'));
-    
-}
