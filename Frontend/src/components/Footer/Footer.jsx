be8bfb66753a926ff4c0beac3c17eea347eecb38
hoang1904 <dinhviethoang1904@gmail.com>
2025-05-02 22:32:29 +0700

    add map

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -19,18 +19,27 @@ const Footer = () => {
         <div className="footer-content-center">
             <h3>Company</h3>
             <ul>
-              <li>Home</li>
-              <li>About us</li>
-              <li>Delivery</li>
-              <li>Privacy policy</li>
+              <li key="home">Home</li>
+              <li key="about-us">About us</li>
+              <li key="delivery">Delivery</li>
+              <li key="privacy-policy">Privacy policy</li>
             </ul>
         </div>
         <div  className="footer-content-right">
             <h3>Contact us</h3>
             <ul>
-              <li>123-456-7890</li>
-              <li>derectme@tomato.com</li>
-            </ul>  
+              <li key="phone">123-456-7890</li>
+              <li key="email">derectme@tomato.com</li>
+            </ul>
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.8581690910723!2d106.68427047535981!3d10.822164158347766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174deb3ef536f31%3A0x8b7bb8b7c956157b!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2hp4buHcCBUUC5IQ00!5e0!3m2!1svi!2s!4v1746194706335!5m2!1svi!2s"
+              width="400" 
+              height="250" 
+              style={{ border: 0 }} 
+              allowFullScreen 
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>  
         </div>
       </div>
       <hr />
